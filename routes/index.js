7a1ed5196c1fbf0e98b7179859737011314282c4
jaydson <jaydson@jaydson.(none)>
2012-06-24 03:23:20 -0300

    Start

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res) {
+  res.render('index', { title: 'MyScrap' })
+};
+
+exports.scraper = function(req, res) {
+	var scraper = require('scraper'),
+		url = req.body.url,
+		selector = req.body.selector,
+		type = req.body.type;
+	
+    console.log('Scraping '+ url );
+	scraper(url, function (err, $) {
+        var types = {
+            json : 'text/json',
+            html : 'text/plain'
+        },
+        htmlString = $(selector).html(),
+        data = '';
+        
+        res.header('Content-Type', types[type]);
+        if (type === 'json') {
+            data = { data : htmlString };
+        } else if (type === 'html') {
+            data = htmlString;
+        }        
+
+        if(data !== '') {
+            res.send(data);
+        }
+	});
+
+};
